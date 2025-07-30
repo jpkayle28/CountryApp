20345c881b56f9abaf079a5ba91e7c32d2b578c5
Jean-Pierre Kayle <jpkayle28@gmail.com>
2025-07-30 23:18:40 +0300

    Removing Main.storyboard & unnecessary files

---
+++ CountryApp/CountryApp/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  CountryApp
+//
+//  Created by Jean-Pierre Kayle on 30/07/2025.
+//
+
+import UIKit
+
+UIApplicationMain(
+    CommandLine.argc,
+    CommandLine.unsafeArgv,
+    nil,
+    NSStringFromClass(AppDelegate.self)
+)
